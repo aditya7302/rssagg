bfbfbf2e9cd7061047abab2c2c4f087793be54a7
aditya7302 <aditya7302@gmail.com>
2024-06-07 14:47:51 +0530

    added handler functions

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main(){
-	fmt.Println("hello world")
 
 	godotenv.Load(".env")
 
@@ -20,6 +21,28 @@ func main(){
 	}
 
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*","http://*"},
+		AllowedMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":"+portString,
+	}
+
+	log.Printf("Server starting on port %v",portString)
+	err := srv.ListenAndServe()
+	if err != nil{
+		log.Fatal(err)
+	}
+
 
 	fmt.Println("Port:",portString)
 }
