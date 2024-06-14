bddbcb189b9ea1f1b82b2eb86791b59d9dba840e
aditya7302 <aditya7302@gmail.com>
2024-06-14 15:59:58 +0530

    added feed_follows

--- handler_feed.go
+++ handler_feed.go
@@ -41,3 +41,15 @@ func (apiCfg *apiConfig)handlerCreateFeed(w http.ResponseWriter,r *http.Request,
 	}
 	respondWithJSON(w,201,databaseFeedToFeed(feed))
 }
+
+func (apiCfg *apiConfig)handlerGetFeeds(w http.ResponseWriter,r *http.Request){
+
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+
+	if err != nil{
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get feed: %v",err))
+		return
+	}
+	respondWithJSON(w,201,databaseFeedsToFeeds(feeds))
+}
+
